89752bfb5842392c9dfc55c538f9b6053317b167
Mohammad Nazhiif <nazhiifalghoniy@gmail.com>
2025-06-22 00:40:45 +0700

    fix: category duplication by removing factory usage in seeders and linking products to existing categories

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -6,8 +6,6 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 uses(RefreshDatabase::class);
 
 beforeEach(function () {
-    // Jalankan migrasi dan buat data dummy produk
-    Product::factory()->count(4)->create();
 });
 
 it('returns a successful response', function () {
